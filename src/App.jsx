623f85b1fd71850eaf579fd5174f20cdae1d0d1a
chikku5656 <uksujjawalkumar@gmail.com>
2024-08-06 16:26:24 +0530

    updated 3rd

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,11 @@ function App() {
 
   return (
     <>
-    <Routes></Routes>
+    {/* <Routes> */}
+    <NavBar/>
+      <HomePage/>
+      <Page3rd/>
+    {/* </Routes> */}
     </>
      
 
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,4 +1,5 @@
 import H1 from '../assets/logo1.png';
+
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
