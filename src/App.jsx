0dacce230aa2504ccfb731f1903829775be01fe3
vipin kumar <vipinkumar721>
2024-08-06 15:43:26 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+
+
 import './App.css'
-import { Route, Routes } from 'react-router'
-import HomePage from './pages/HomePage/HomePage'
-// import Page3rd from './pages/HomePage/Page3rd'
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-     <Routes>
-      <Route path='/' element={<HomePage/>}/>
-      {/* <Route path='/' element={<Page3rd/>}/> */}
-     </Routes>
+    
     </>
   )
 }
