0f96a705276bd9dc40f8dbff4e1b6b24bc8c9dca
vipin kumar <vipinkumar721>
2024-08-06 15:42:03 +0530

    Homepage done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage/HomePage'
-import Page3rd from './pages/HomePage/Page3rd'
+// import Page3rd from './pages/HomePage/Page3rd'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,8 +12,8 @@ function App() {
   return (
     <>
      <Routes>
-      {/* <Route path='/' element={<HomePage/>}/> */}
-      <Route path='/' element={<Page3rd/>}/>
+      <Route path='/' element={<HomePage/>}/>
+      {/* <Route path='/' element={<Page3rd/>}/> */}
      </Routes>
     </>
   )
