2a5611eb3bc00066288eb45c4c0957e14598e2f1
chikku5656 <uksujjawalkumar@gmail.com>
2024-08-06 11:25:39 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage/HomePage'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Routes>
+      <Route path='/' element={<HomePage/>}/>
+     </Routes>
+    </>
+  )
+}
+
+export default App
